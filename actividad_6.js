bccdd09ae729dc80cd8188e8a3b0462f3a043b2c
@mauricio-jacki <mauriciojacki@gmail.com>
2025-04-14 15:42:44 -0300

    agrego archivo y termino actividad 6

---
+++ actividad_6.js
@@ -0,0 +1,5 @@
+let x = 6; // asignacion de entero 1
+let y = 10 // asignacion de entero 2 y numero limite
+for (let x = 6; x <= y; x++) {
+    console.log(x); //imprime los elementos que los separan
+}
