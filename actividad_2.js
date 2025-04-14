c471f90ede408ca2759705bd667dd20a23e2cd99
@mauricio-jacki <mauriciojacki@gmail.com>
2025-04-14 14:23:03 -0300

    añadio archivo y actualizo actividad_2

--- actividad_2.js
+++ actividad_2.js
@@ -1,8 +1,11 @@
 let x = 10; // valor 1
+console.log(x);
 
 let y = 6; // valor 2
+console.log(y);
 
 let z = 4; // valor 3
+console.log(z);
 
 if (x>y) {
     console.log( x,"es el mayor") // indica que el numero ingresado es el mayor
