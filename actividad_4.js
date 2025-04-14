851251afb6fea73904e82f71a345fc469168f4b2
@mauricio-jacki <mauriciojacki@gmail.com>
2025-04-14 15:05:09 -0300

    termino actividad 4

---
+++ actividad_4.js
@@ -0,0 +1,11 @@
+var x = 2;
+let y = 2;
+var resultado = 0
+console.log(x);
+function dividir(x,y){
+    let resultado = x / y;
+    return resultado;
+}
+if (resultado == 0) {
+    console.log("el numero es par")
+}
