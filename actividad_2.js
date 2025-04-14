62c0af8d8805194b432c2df534d0d31a1eabb2c8
@mauricio-jacki <mauriciojacki@gmail.com>
2025-04-14 14:20:07 -0300

    añadio archivo y termino actividad 2

---
+++ actividad_2.js
@@ -0,0 +1,12 @@
+let x = 10; // valor 1
+
+let y = 6; // valor 2
+
+let z = 4; // valor 3
+
+if (x>y) {
+    console.log( x,"es el mayor") // indica que el numero ingresado es el mayor
+}
+if (z<y) {
+    console.log( z,"es el menor") // indica que el numero ingresado es el menor
+}
