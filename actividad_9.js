93621ea752cdf06a9f781003e2d0c9912a47dd1f
@mauricio-jacki <mauriciojacki@gmail.com>
2025-04-14 16:23:58 -0300

    añadio archivo

---
+++ actividad_9.js
@@ -0,0 +1,7 @@
+const numeros = ["num1","num2","num3","num4","num5"]
+console.log("numeros:", numeros);
+console.log("cantidad de numeros:",numeros.lenght);
+console.log("lista de elementos:",numeros);
+for (let numeros = 0; numeros <=numeros.lenght; i++) {
+    console.log(numeros[i]);
+}
