84d5dbafcd89ef9e439ff406974e4d7940a45d7e
@mauricio-jacki <mauriciojacki@gmail.com>
2025-04-14 15:25:45 -0300

    añadio archivo y termino actividad 5

---
+++ actividad_5.js
@@ -0,0 +1,6 @@
+var cantpalabras = cantpalabras; // asigna la cantidad de palabras
+let palabra = "pala" // asigna la palabra
+let numero = 8; // asigna el numero maximo de palabras
+for (let cantpalabras= 0; cantpalabras <=numero; cantpalabras++) { //realiza el bucle para
+    console.log("la palabra",palabra,"se repite",cantpalabras,"veces"); 
+}
